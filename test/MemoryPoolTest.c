fef1070ddc63fb5929fecdb33e1c8f00c2a6ef40
Nicholas Draper <nickd2225@gmail.com>
2023-05-09 20:02:53 -0700

    Improved mem pool tests. Added new lines to files at the end

--- test/MemoryPoolTest.c
+++ test/MemoryPoolTest.c
@@ -3,16 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+void helperTestPoolIsCleanedUp(MemoryPoolList* pool)
 {
-    printf("STARTING MEMORY POOL TEST\n");
+    assert(pool->number_of_nodes == 0);
+    assert(pool->head == NULL);
+    assert(pool->tail == NULL);
+}
 
+void testZeroNodePool()
+{
     MemoryPoolList pool;
     initializeMemoryPool(&pool);
 
     assert(pool.number_of_nodes == 0);
     assert(pool.head == NULL);
     assert(pool.tail == NULL);
+
+    destroyMemoryPool(&pool);
+    helperTestPoolIsCleanedUp(&pool);
+}
+
+void testSingleNodePool()
+{
+    MemoryPoolList pool;
+    initializeMemoryPool(&pool);
 
     void* mem_block = malloc(5);
     addMemoryPoolNode(&pool, 5, mem_block);
@@ -21,9 +35,15 @@ int main()
     assert(pool.head != NULL);
     assert(pool.tail != NULL);
 
-    void* mem_block_2 = malloc(10);
-    addMemoryPoolNode(&pool, 10, mem_block_2);
-    assert(pool.number_of_nodes == 2);
-
     destroyMemoryPool(&pool);
+    helperTestPoolIsCleanedUp(&pool);
+}
+
+int main()
+{
+    printf("STARTING MEMORY POOL TEST\n");
+
+    testZeroNodePool();
+
+    testSingleNodePool();
 }
