90f3001188193a90b0b87f76174920708da62d96
Nicholas Draper <nickd2225@gmail.com>
2023-05-23 22:41:13 -0700

    Updated get value message to not use memory pool

--- test/messages/GetValueMessageRequestTest.c
+++ test/messages/GetValueMessageRequestTest.c
@@ -1,6 +1,7 @@
 #include "messages/GetValueMessageRequest.h"
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 void testCalculateGetValueMessageRequestSize()
 {
@@ -19,9 +20,6 @@ void testCalculateGetValueMessageRequestSize()
 
 void testSerializeDeserializeGetValueMessageRequest()
 {
-    MemoryPoolList pool;
-    initializeMemoryPool(&pool);
-
     GetValueMessageRequest message = {
         .key = &(AugmentedBuffer) {
             .buffer_pointer = "test_key",
@@ -29,23 +27,33 @@ void testSerializeDeserializeGetValueMessageRequest()
         }
     };
 
+    // Get message size
     uint32_t message_size;
     calculateGetValueMessageRequestSize(&message, &message_size);
-    AugmentedBuffer buffer;
-    allocateMemoryInPool(&pool, message_size, &buffer);
-    serializeGetValueMessageRequest(&message, &buffer);
+
+    // Create a buffer that can hold the message size.
+    AugmentedBuffer serialized_buffer;
+    serialized_buffer.buffer_pointer = malloc(message_size);
+    serialized_buffer.buffer_size = message_size;
+    serializeGetValueMessageRequest(&message, &serialized_buffer);
+
+    // Create a result message that can hold all the deserialized data.
     GetValueMessageRequest result_message;
-    deserializeGetValueMessageRequest(&pool, &buffer, &result_message);
+    result_message.key = malloc(sizeof(AugmentedBuffer));
+    result_message.key->buffer_pointer = malloc(8);
+    result_message.key->buffer_size = 8;
+    deserializeGetValueMessageRequest(&serialized_buffer, &result_message);
 
     assert(areAugmentedBuffersSame(result_message.key, message.key));
     
-    destroyMemoryPool(&pool);
+    free(result_message.key->buffer_pointer);
+    free(result_message.key);
+    free(serialized_buffer.buffer_pointer);
 }
 
 int main()
 {
     printf("STARTING GET VALUE VALUE MESSAGE REQUEST TEST\n");
-
     testCalculateGetValueMessageRequestSize();
 
     testSerializeDeserializeGetValueMessageRequest();
