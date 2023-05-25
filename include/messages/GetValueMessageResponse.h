c2f13d285772ab930bdc6c5457d6b538bb31b879
Nicholas Draper <nickd2225@gmail.com>
2023-05-24 17:20:01 -0700

    Updated GetValueMessageResponse to not use a memory pool.

--- include/messages/GetValueMessageResponse.h
+++ include/messages/GetValueMessageResponse.h
@@ -19,6 +19,20 @@ void calculateGetValueMessageResponseSize(GetValueMessageResponse* message, uint
 
 void serializeGetValueMessageResponse(GetValueMessageResponse* message, AugmentedBuffer* return_buffer);
 
-void deserializeGetValueMessageResponse(MemoryPoolList* pool, AugmentedBuffer* buffer, GetValueMessageResponse* return_message);
+/**
+ * @brief Deserializes a byte buffer `buffer` into a get value message response `return_message`.
+ * 
+ * This function expects `return_message` to be properly allocated and the size of the buffer to be set
+ * before hand.
+ * 
+ * - GetValueMessageResponse (return_message)
+ *   - AugmentedBuffer* (buffer) [needs to be allocated]
+ *     - void* (buffer_pointer) [needs to be allocated]
+ *     - uint32_t (buffer_size) [needs to be allocated and set to the expected size]
+ * 
+ * @param buffer 
+ * @param return_message 
+ */
+void deserializeGetValueMessageResponse(AugmentedBuffer* buffer, GetValueMessageResponse* return_message);
 
 #endif
